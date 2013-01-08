55b712aa890d6d3283ff32535920be53f2f1b573
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-08 07:31:46 +0000

    testing oauth flow

--- app/controller/admin_controller.py
+++ app/controller/admin_controller.py
@@ -2,13 +2,26 @@ import config
 
 from apiclient.discovery import build
 from app.controller.base_controller import BaseController
-from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import OAuth2Decorator
+
+# set up decorator
+decorator = OAuth2Decorator(
+  client_id=config.client_id,
+  client_secret=config.client_secret,
+  scope='https://docs.googleusercontent.com/ https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/userinfo.email',
+  redirect_uri=config.redirect_uri
+)
 
 class AdminController(BaseController):
-  def get(self, callback):
+  @decorator.oauth_required
+  def dispatch(self):
     # set up our client
     self._set_up_client()
 
+    # Dispatch the request.
+    webapp2.RequestHandler.dispatch(self)
+
+  def get(self, callback):
     # load edit view
     self._edit()
 
@@ -17,18 +30,16 @@ class AdminController(BaseController):
     folder_contents = None
 
     if folder is not None:
-      folder_contents = self._client.files.get(folderId=folder).execute()
+      folder_contents = self._client.files.get(folderId=folder).execute(http=self._http)
 
     # get the root folders
-    root_folders = self._client.files.get().execute()
+    root_folders = self._client.files.get().execute(http=self._http)
 
     self.response.out.write(root_folders)
 
   def _set_up_client(self):
-    # set up flow
-    self._flow = OAuth2WebServerFlow(
-      client_id=config.client_id,
-      client_secret=config.client_secret,
-      scope='https://docs.googleusercontent.com/ https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/userinfo.email',
-      redirect_uri=config.redirect_uri
-    )
+    # get http obj
+    self._http = decorator.http()
+
+    # build service
+    self._client = build('drive', 'v2');
