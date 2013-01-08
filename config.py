de0d9c880ee8982a9bddfe7e778c032bfc0c015f
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-08 19:45:24 +0000

    removed config

--- config.py
+++
@@ -1,4 +0,0 @@
-# get all this from your google api console: https://code.google.com/apis/console
-client_id = '700099067861.apps.googleusercontent.com'
-client_secret = '7Hj5CliVKUa9NDarqe5VND-R'
-developer_key = 'AIzaSyCTLbMfk1f6b5hgTNKxUROqhCUsRGOMKC0'
